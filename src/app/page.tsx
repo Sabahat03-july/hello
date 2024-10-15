dd3a94bf9cde3b244443b034a6c1a4217582399f
Sabahat03-july <sabahatsabahat1999@gmail.com>
2024-10-15 23:48:42 +0500

    Iinitial commit of my hello world

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+  <div className="flex  bg-slate-500 justify-between mt-20 mx-5 ">
+    <h1 className="mt-6 border-l-2 pl-6 italic  ">
+      my first milestone 
+      <p>
+    My name is <span className=" text-orange-500">Sabahat </span>, and I am a student of the Governor IT initiative. My slot is from 7 to 10 on Wednesdays. The class teacher is <span className="text-orange-500">Sir Mubashir Ali</span>, and my roll number is <span className="text-orange-500">104073</span>. <span className="text-orange-500">Sir Ameen Alam</span> has assigned this milestone, and this is my first milestone.
+      -
+      </p>
+    </h1>
+    
+  </div>
+  );
+}
